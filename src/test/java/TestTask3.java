8376e9a4d323fec3e941f05400cd9e0c25271a6b
Danya Bykov <bykov.danya.ua@gmail.com>
2024-11-25 11:43:00 +0200

    fix

--- src/test/java/TestTask3.java
+++ src/test/java/TestTask3.java
@@ -5,7 +5,7 @@ import ucu.edu.ua.Task3.*;
 public class TestTask3 {
     @Test
     public void testProxy() {
-        MyImage image = new ProxyImage("image.jpg");
+        MyImage image = new ProxyImage("./test.png");
         image.display();
         assertEquals(true, image instanceof ProxyImage);
     }
