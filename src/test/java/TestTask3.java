091b4cc00386ce89a223f2e6bc9f2c6c118cd5b7
Danya Bykov <bykov.danya.ua@gmail.com>
2024-11-25 11:48:57 +0200

    fix

--- src/test/java/TestTask3.java
+++ src/test/java/TestTask3.java
@@ -1,11 +1,14 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.doNothing;
 
 import ucu.edu.ua.Task3.*;
 public class TestTask3 {
     @Test
-    public void testProxy() {
-        MyImage image = new ProxyImage("./test.png");
+    public void testProxyImage() {
+        MyImage image = mock(ProxyImage.class);
+        doNothing().when(image).display();
         image.display();
         assertEquals(true, image instanceof ProxyImage);
     }
